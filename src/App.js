d9dce2ed577e2a0986b283c98f7d3b501d3fbd6f
unknown <himanshu25031991@gmail.com>
2020-10-31 19:38:02 +0530

    useCallback Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import './App.css';
-import DataFetchingOne from './components/DataFetchingOne';
-import DataFetchingTwo from './components/DataFetchingTwo';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className='App'>
-      <DataFetchingTwo />
-      <DataFetchingOne />
+      <ParentComponent />
     </div>
   );
 }
