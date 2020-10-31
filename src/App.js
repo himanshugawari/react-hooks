f3f024c9d136d11ffb85a577733140f90ce97596
unknown <himanshu25031991@gmail.com>
2020-10-31 23:54:36 +0530

    useRef Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import Counter from './components/Counter';
+import ClassTimer from './components/ClassTimer';
+import FocusInput from './components/FocusInput';
+import HookTimer from './components/HookTimer';
 
 function App() {
   return (
     <div className='App'>
-      <Counter />
+      <FocusInput />
+      <ClassTimer />
+      <HookTimer />
     </div>
   );
 }
