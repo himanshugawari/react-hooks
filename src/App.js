bdff62daf3b7c4f641b3ac9d4bce26afc1e16a96
unknown <himanshu25031991@gmail.com>
2020-11-01 14:55:24 +0530

    useCounter Custom Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import DocTitleOne from './components/DocTitleOne';
-import DocTitleTwo from './components/DocTitleTwo';
+import Counter1 from './components/Counter1';
+import Counter2 from './components/Counter2';
 
 function App() {
   return (
     <div className='App'>
-      <DocTitleOne />
-      <DocTitleTwo />
+      <Counter1 />
+      <Counter2 />
     </div>
   );
 }
